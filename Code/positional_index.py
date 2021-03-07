0e2653f0be872fed48f3dc4ba1d49c9b4c084a6b
StijnDesloovere <57724700+StijnDesloovere@users.noreply.github.com>
2021-03-07 11:40:47 +0100

    Added positional_index function

---
+++ Code/positional_index.py
@@ -0,0 +1,37 @@
+import os
+
+
+# Create a positional index that stores the positions of a word in each document that contains the word
+def create_positional_index():
+    positional_index = {}
+    document = 1
+
+    for filename in os.listdir('../Documents'):
+        file = open('../Documents/' + filename, "r")
+        position = 0
+
+        for line in file:
+            for word in line.split():
+                # Remove special characters in sentences
+                word = word.replace(',', '').replace('.', '').replace('"', '').lower()
+                # Add a new word to the positional index
+                if word not in positional_index:
+                    positional_index[word] = {}
+                # Add a new document entry to the word dictionary in the positional index
+                if document not in positional_index[word]:
+                    positional_index[word][document] = []
+
+                positional_index[word][document].append(position)
+
+                position += 1
+
+        document += 1
+
+    return positional_index
+
+
+index = create_positional_index()
+print(len(index['software'][14]))
+
+
+
